e5597ffed9a646afb9efe72f3be1bba0f0df7d09
ljzn <rubyminer@foxmail.com>
2016-05-24 21:09:18 +0800

    new

--- highlight.rb
+++ highlight.rb
@@ -5,13 +5,12 @@ lines = File::open( name + ".markdown" ).read
 
 i = 0
 
-# lines.each do |line|
 while lines.include?("```") 
  	if i.even?
   		lines.sub!( "```", "{% highlight ruby %}" )
   		i += 1
   	else
-  		lines.sub!( "```", "{% end highlight %}" )
+  		lines.sub!( "```", "{% endhighlight %}" )
   		i += 1
   	end
 end
@@ -20,20 +19,5 @@ p lines
 
 File.open( name + ".markdown", "w") { |file| file << lines }
 
-# 		if i.even?
-# 			line = "{% highlight ruby %}"
-# 			i += 1
-# 		else
-# 			line = "{% endhighlight %}"
-# 			i += 1
-# 		end
-# 	end
-# end
-
-# p lines
-
-#File.open( name + ".markdown","w" ) do |f|
-#	f.write(lines)
-#end
 
 
